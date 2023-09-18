e04d7b00b1379b1a922036a01e7e400666d26ed3
James Gichia <jamesgichia15@gmail.com>
2023-09-18 15:26:28 +0300

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
+
 /**
  * swap_int - check the code
  * Swaps the the values of @a and @b
  * Has no return value
-*/
+ */
 
 void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
