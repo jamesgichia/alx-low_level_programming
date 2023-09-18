42b8d26b23f28249bf4232f39565e6f124caad6c
James Gichia <jamesgichia15@gmail.com>
2023-09-18 15:04:27 +0300

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
 }
 
+/**
+ * reset_to_98 - check the code
+ * @n: n is a pointer
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
