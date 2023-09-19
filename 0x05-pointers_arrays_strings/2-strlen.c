3bc0d5a6418cf2cfcd8c16adfbb3c509943b22fc
James Gichia <jamesgichia15@gmail.com>
2023-09-19 08:31:59 +0300

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,10 @@
  * _strlen - takes a string and returns its length
  * @s : is a pointer pointing to the address of the
  *  first character of the string
- * The function returns the length of the string
+ * The function returns the @length of the string
+ *
+ * @length is the return value of the functio.
+ * It is the length of the array passed to the function
  */
 
 int _strlen(char *s)
