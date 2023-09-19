f9463f30fe86dc6e0cce526c4f9492ec53b8da4b
James Gichia <jamesgichia15@gmail.com>
2023-09-19 08:28:41 +0300

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _strlen - takes a string and returns its length
- * @s : is a pointer pointing to the address of the first character of the string
+ * @s : is a pointer pointing to the address of the
+ *  first character of the string
  * The function returns the length of the string
  */
 
@@ -14,5 +15,5 @@ int _strlen(char *s)
 		length++;
 	}
 
-	return length;
+	return (length);
 }
